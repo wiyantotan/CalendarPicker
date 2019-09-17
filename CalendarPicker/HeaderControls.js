678b886210ff268619770ea506f77a6a2a696e0d
wiyantotan <wiyantotan@gmail.com>
2019-09-17 21:02:56 +0700

    custom style

--- CalendarPicker/HeaderControls.js
+++ CalendarPicker/HeaderControls.js
@@ -1,68 +1,78 @@
 import React from 'react';
 import {
-  View,
-  Text,
+    View,
+    Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { Utils } from './Utils';
 import Controls from './Controls';
 
 export default function HeaderControls(props) {
-  const {
-    styles,
-    currentMonth,
-    currentYear,
-    onPressNext,
-    onPressPrevious,
-    months,
-    previousTitle,
-    nextTitle,
-    textStyle,
-      prevComponent,
-      nextComponent
-  } = props;
-  const MONTHS = months? months : Utils.MONTHS; // English Month Array
-  // getMonth() call below will return the month number, we will use it as the
-  // index for month array in english
-  const previous = previousTitle ? previousTitle : 'Previous';
-  const next = nextTitle ? nextTitle : 'Next';
-  const month = MONTHS[currentMonth];
-  const year = currentYear;
+    const {
+        styles,
+        currentMonth,
+        currentYear,
+        onPressNext,
+        onPressPrevious,
+        months,
+        previousTitle,
+        nextTitle,
+        textStyle,
+        PrevComponent,
+        NextComponent
+    } = props;
+    const MONTHS = months? months : Utils.MONTHS; // English Month Array
+    // getMonth() call below will return the month number, we will use it as the
+    // index for month array in english
+    const previous = previousTitle ? previousTitle : 'Previous';
+    const next = nextTitle ? nextTitle : 'Next';
+    const month = MONTHS[currentMonth];
+    const year = currentYear;
 
-  return (
-    <View style={styles.headerWrapper}>
-      <View>
-        <Text style={[styles.monthLabel, textStyle]}>
-           { month } { year }
-        </Text>
-      </View>
-        {
-            prevComponent? (<prevComponent onPress={onPressPrevious} />) : (
-                <Controls
-                    label={previous}
-                    onPressControl={onPressPrevious}
-                    styles={[styles.monthSelector, styles.prev]}
-                    textStyles={textStyle}
-                />
-            )
-        }
-        {
-            nextComponent? (<nextComponent onPress={onPressNext} />) : (
-                <Controls
-                    label={next}
-                    onPressControl={onPressNext}
-                    styles={[styles.monthSelector, styles.next]}
-                    textStyles={textStyle}
-                />
-            )
-        }
-    </View>
-  );
+    return (
+        <View style={styles.headerWrapper}>
+            <View style={{flex: 1}}>
+                <Text style={[styles.monthLabel, textStyle]}>
+                    { month } { year }
+                </Text>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', paddingRight: 20, paddingBottom: 5}}>
+                {
+                    PrevComponent? (
+                        <View style={{borderRadius: 50, backgroundColor: '#E6F1FF', marginRight: 10}}>
+                            <PrevComponent type="MaterialCommunityIcons" name={'chevron-left'} size={22} color={'#545DFF'} onPress={onPressPrevious} />
+                        </View>
+                    ) : (
+                        <Controls
+                            label={previous}
+                            onPressControl={onPressPrevious}
+                            styles={[styles.monthSelector, styles.prev]}
+                            textStyles={textStyle}
+                        />
+                    )
+                }
+                {
+                    NextComponent? (
+                        <View style={{borderRadius: 50, backgroundColor: '#E6F1FF'}}>
+                            <NextComponent type="MaterialCommunityIcons" name={'chevron-right'} size={22} color={'#545DFF'} onPress={onPressNext} />
+                        </View>
+                    ) : (
+                        <Controls
+                            label={next}
+                            onPressControl={onPressNext}
+                            styles={[styles.monthSelector, styles.next]}
+                            textStyles={textStyle}
+                        />
+                    )
+                }
+            </View>
+        </View>
+    );
 }
 
 HeaderControls.propTypes = {
-  currentMonth: PropTypes.number,
-  currentYear: PropTypes.number,
-  onPressNext: PropTypes.func,
-  onPressPrevious: PropTypes.func,
+    currentMonth: PropTypes.number,
+    currentYear: PropTypes.number,
+    onPressNext: PropTypes.func,
+    onPressPrevious: PropTypes.func,
 };
--- CalendarPicker/makeStyles.js
+++ CalendarPicker/makeStyles.js
@@ -9,174 +9,176 @@ const DEFAULT_SELECTED_TEXT_COLOR = '#000000';
 const DEFAULT_TODAY_BACKGROUND_COLOR = '#CCCCCC';
 
 function getBorderRadiusByShape(scaler, dayShape) {
-  if (dayShape === 'square') {
-    return 0;
-  } else {
-    return 30*scaler;
-  }
+    if (dayShape === 'square') {
+        return 0;
+    } else {
+        return 30*scaler;
+    }
 }
 
 export function makeStyles(scaler, backgroundColor, textColor, todayBackgroundColor, dayShape) {
-  const SELECTED_BG_COLOR = backgroundColor ? backgroundColor : DEFAULT_SELECTED_BACKGROUND_COLOR;
-  const SELECTED_TEXT_COLOR = textColor ? textColor : DEFAULT_SELECTED_TEXT_COLOR;
-  const TODAY_BG_COLOR = todayBackgroundColor ? todayBackgroundColor : DEFAULT_TODAY_BACKGROUND_COLOR;
+    const SELECTED_BG_COLOR = backgroundColor ? backgroundColor : DEFAULT_SELECTED_BACKGROUND_COLOR;
+    const SELECTED_TEXT_COLOR = textColor ? textColor : DEFAULT_SELECTED_TEXT_COLOR;
+    const TODAY_BG_COLOR = todayBackgroundColor ? todayBackgroundColor : DEFAULT_TODAY_BACKGROUND_COLOR;
 
-  return {
-    calendar: {
-      height: 320*scaler,
-      marginTop: 10*scaler
-    },
+    return {
+        calendar: {
+            height: 320*scaler,
+            marginTop: 10*scaler
+        },
 
-    dayButton: {
-      width: 30*scaler,
-      height: 30*scaler,
-      borderRadius: getBorderRadiusByShape(scaler, dayShape),
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        dayButton: {
+            width: 30*scaler,
+            height: 30*scaler,
+            borderRadius: getBorderRadiusByShape(scaler, dayShape),
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    dayLabel: {
-      fontSize: 14*scaler,
-      color: '#000',
-      alignSelf: 'center'
-    },
+        dayLabel: {
+            fontSize: 14*scaler,
+            color: '#000',
+            alignSelf: 'center'
+        },
 
-    selectedDayLabel: {
-      color: SELECTED_TEXT_COLOR,
-    },
+        selectedDayLabel: {
+            color: SELECTED_TEXT_COLOR,
+        },
 
-    dayLabelsWrapper: {
-      flexDirection: 'row',
-      borderBottomWidth: 1,
-      borderTopWidth: 1,
-      paddingTop: 10*scaler,
-      paddingBottom: 10*scaler,
-      alignSelf: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'rgba(0,0,0,0.0)',
-      borderColor: 'rgba(0,0,0,0.2)'
-    },
+        dayLabelsWrapper: {
+            flexDirection: 'row',
+            borderBottomWidth: 1,
+            borderTopWidth: 1,
+            paddingTop: 10*scaler,
+            paddingBottom: 10*scaler,
+            alignSelf: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'rgba(0,0,0,0.0)',
+            borderColor: 'rgba(0,0,0,0.2)'
+        },
 
-    daysWrapper: {
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        daysWrapper: {
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    dayLabels: {
-      width: 50*scaler,
-      fontSize: 12*scaler,
-      color: '#000',
-      textAlign: 'center'
-    },
+        dayLabels: {
+            width: 50*scaler,
+            fontSize: 12*scaler,
+            color: '#000',
+            textAlign: 'center'
+        },
 
-    selectedDay: {
-      width: 30*scaler,
-      height:30*scaler,
-      borderRadius: getBorderRadiusByShape(scaler, dayShape),
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        selectedDay: {
+            width: 30*scaler,
+            height:30*scaler,
+            borderRadius: getBorderRadiusByShape(scaler, dayShape),
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    selectedDayBackground: {
-      backgroundColor: SELECTED_BG_COLOR,
-    },
+        selectedDayBackground: {
+            backgroundColor: SELECTED_BG_COLOR,
+        },
 
-    selectedToday: {
-      width: 30*scaler,
-      height:30*scaler,
-      backgroundColor: TODAY_BG_COLOR,
-      borderRadius: getBorderRadiusByShape(scaler, dayShape),
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        selectedToday: {
+            width: 30*scaler,
+            height:30*scaler,
+            backgroundColor: TODAY_BG_COLOR,
+            borderRadius: getBorderRadiusByShape(scaler, dayShape),
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    dayWrapper: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 50*scaler,
-      height: 40*scaler,
-      backgroundColor: 'rgba(0,0,0,0.0)'
-    },
+        dayWrapper: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 50*scaler,
+            height: 40*scaler,
+            backgroundColor: 'rgba(0,0,0,0.0)'
+        },
 
-    startDayWrapper: {
-      width: 50*scaler,
-      height: 30*scaler,
-      borderTopLeftRadius: 20*scaler,
-      borderBottomLeftRadius: 20*scaler,
-      backgroundColor: SELECTED_BG_COLOR,
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        startDayWrapper: {
+            width: 50*scaler,
+            height: 30*scaler,
+            borderTopLeftRadius: 20*scaler,
+            borderBottomLeftRadius: 20*scaler,
+            backgroundColor: SELECTED_BG_COLOR,
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    endDayWrapper: {
-      width: 50*scaler,
-      height: 30*scaler,
-      borderTopRightRadius: 20*scaler,
-      borderBottomRightRadius: 20*scaler,
-      backgroundColor: SELECTED_BG_COLOR,
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        endDayWrapper: {
+            width: 50*scaler,
+            height: 30*scaler,
+            borderTopRightRadius: 20*scaler,
+            borderBottomRightRadius: 20*scaler,
+            backgroundColor: SELECTED_BG_COLOR,
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    inRangeDay: {
-      width: 50*scaler,
-      height: 30*scaler,
-      backgroundColor: SELECTED_BG_COLOR,
-      alignSelf: 'center',
-      justifyContent: 'center'
-    },
+        inRangeDay: {
+            width: 50*scaler,
+            height: 30*scaler,
+            backgroundColor: SELECTED_BG_COLOR,
+            alignSelf: 'center',
+            justifyContent: 'center'
+        },
 
-    monthLabel: {
-      fontSize: 16*scaler,
-      color: '#000',
-      marginBottom: 10*scaler,
-      width: 180*scaler,
-      textAlign: 'center'
-    },
+        monthLabel: {
+            fontSize: 16*scaler,
+            color: '#000',
+            marginBottom: 10*scaler,
+            width: 180*scaler,
+            textAlign: 'center',
+            fontWeight: 'bold'
+        },
 
-    headerWrapper: {
-      alignItems: 'center',
-      flexDirection: 'row',
-      alignSelf: 'center',
-      padding: 5*scaler,
-      paddingBottom: 3*scaler,
-      backgroundColor: 'rgba(0,0,0,0.0)'
-    },
+        headerWrapper: {
+            alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            // alignSelf: 'center',
+            // padding: 5*scaler,
+            paddingBottom: 3*scaler,
+            backgroundColor: 'rgba(0,0,0,0.0)'
+        },
 
-    monthSelector: {
-      marginBottom: 10*scaler,
-      fontSize: 14*scaler,
-      width: 80*scaler
-    },
+        monthSelector: {
+            marginBottom: 10*scaler,
+            fontSize: 14*scaler,
+            width: 80*scaler
+        },
 
-    prev: {
-      textAlign: 'left'
-    },
+        prev: {
+            textAlign: 'left'
+        },
 
-    next: {
-      textAlign: 'right'
-    },
+        next: {
+            textAlign: 'right'
+        },
 
-    yearLabel: {
-      fontSize: 14*scaler,
-      fontWeight: 'bold',
-      color: '#000',
-      textAlign: 'center'
-    },
+        yearLabel: {
+            fontSize: 14*scaler,
+            fontWeight: 'bold',
+            color: '#000',
+            textAlign: 'center'
+        },
 
-    weeks: {
-      flexDirection: 'column'
-    },
+        weeks: {
+            flexDirection: 'column'
+        },
 
-    weekRow: {
-      flexDirection: 'row'
-    },
+        weekRow: {
+            flexDirection: 'row'
+        },
 
-    disabledText: {
-      fontSize: 14*scaler,
-      color: '#BBBBBB',
-      alignSelf: 'center',
-      justifyContent: 'center'
-    }
-  };
+        disabledText: {
+            fontSize: 14*scaler,
+            color: '#BBBBBB',
+            alignSelf: 'center',
+            justifyContent: 'center'
+        }
+    };
 }
